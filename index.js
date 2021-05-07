78969b42dc37fa7cd70fa7c497fd602f9db1747d
alex <alexanderstapp@gmail.com>
2021-05-07 15:34:25 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+
+const app = express()
+
+const {SERVER_PORT, CONNECTION_STRING} = process.env
+
+app.use(express.json())
+
+massive({
+    connectionString = CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(dbInstance => app.set('db', dbInstance)).catch(err => console.log(err))
+
+
+app.listen(SERVER_PORT, () => console.log(`listening on ${SERVER_PORT}`))
