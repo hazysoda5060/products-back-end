48c8e7b21f477c57373e1bbb6cc19e7712d854b1
alex <alexanderstapp@gmail.com>
2021-05-11 23:49:52 -0600

    setting up controllers

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const app = express()
 
 const {SERVER_PORT, CONNECTION_STRING} = process.env
 
-app.use(express.json())
-
 massive({
     connectionString = CONNECTION_STRING,
     ssl: {rejectUnauthorized: false}
 }).then(dbInstance => app.set('db', dbInstance)).catch(err => console.log(err))
 
+app.use(express.json())
+
+app.post('/api/products', products_controller.create);
+app.get('/api/products', products_controller.getAll);
+app.get('/api/products/:id', products_controller.getOne);
+app.put('/api/products/:id', products_controller.update);
+app.delete('/api/products/:id', products_controller.delete);
 
 app.listen(SERVER_PORT, () => console.log(`listening on ${SERVER_PORT}`))
